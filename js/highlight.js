1e9f3c4d61e375b9f81e7f44d14133700bfcd779
VIPrepare <64679580+VIPrepare@users.noreply.github.com>
2020-05-02 12:54:45 +0530

    Add files via upload

---
+++ js/highlight.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+
+    $('#a1').hover(function () {
+        $('.test1').css('color', '#67D1BA');
+        $('.test11').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test1').css('color', '#767575');
+                $('.test11').attr('src', 'img/icon1.png');
+            });
+
+
+    $('#a2').hover(function () {
+        $('.test2').css('color', '#67D1BA');
+        $('.test22').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test2').css('color', '#767575');
+                $('.test22').attr('src', 'img/icon1.png');
+            });
+
+    $('#a3').hover(function () {
+        $('.test3').css('color', '#67D1BA');
+        $('.test33').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test3').css('color', '#767575');
+                $('.test33').attr('src', 'img/icon1.png');
+            });
+
+    $('#a4').hover(function () {
+        $('.test4').css('color', '#67D1BA');
+        $('.test44').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test4').css('color', '#767575');
+                $('.test44').attr('src', 'img/icon1.png');
+            });
+
+
+    $('#a5').hover(function () {
+        $('.test5').css('color', '#67D1BA');
+        $('.test55').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test5').css('color', '#767575');
+                $('.test55').attr('src', 'img/icon1.png');
+            });
+
+    $('#a6').hover(function () {
+        $('.test6').css('color', '#67D1BA');
+        $('.test66').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test6').css('color', '#767575');
+                $('.test66').attr('src', 'img/icon1.png');
+            });
+
+    $('#a7').hover(function () {
+        $('.test7').css('color', '#67D1BA');
+        $('.test77').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test7').css('color', '#767575');
+                $('.test77').attr('src', 'img/icon1.png');
+            });
+
+    $('#a8').hover(function () {
+        $('.test8').css('color', '#67D1BA');
+        $('.test88').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test8').css('color', '#767575');
+                $('.test88').attr('src', 'img/icon1.png');
+            });
+
+    $('#a9').hover(function () {
+        $('.test9').css('color', '#67D1BA');
+        $('.test99').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test9').css('color', '#767575');
+                $('.test99').attr('src', 'img/icon1.png');
+            });
+
+    $('#a10').hover(function () {
+        $('.test10').css('color', '#67D1BA');
+        $('.test1010').attr('src', 'img/green.png');
+    },
+            function () {
+                $('.test10').css('color', '#767575');
+                $('.test1010').attr('src', 'img/icon1.png');
+            });
+
+
+
+});
