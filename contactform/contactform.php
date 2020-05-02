43bbaaaf14f9c63d37e51403f5af921c03237e0f
VIPrepare <64679580+VIPrepare@users.noreply.github.com>
2020-05-02 13:42:00 +0530

    Add files via upload

---
+++ contactform/contactform.php
@@ -0,0 +1,12 @@
+
+<?php
+if($_POST){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+	$subject=$_POST['subject']
+    $message = $_POST['message'];
+
+//send email
+    mail("viprepare@gmail.com", "This is an email from:" .$email, $message);
+}
+?>
